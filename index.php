237e0f9b2fb235f8befd115d5b5839050df210ec
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-30 19:51:11 -0300

    pegando dados do front-end e enviando pro back-end

--- index.php
+++ index.php
@@ -18,31 +18,31 @@
 			</div>
 
       		<div class="row">
-      			<div class="col-md-12">
+      	<div class="col-md-12">
   				
-					<div class="card-body font-weight-bold">
-						<form>
-							<div class="form-group">
-								<label for="para">Para</label>
-								<input type="text" class="form-control" id="para" placeholder="joao@dominio.com.br">
-							</div>
-
-							<div class="form-group">
-								<label for="assunto">Assunto</label>
-								<input type="text" class="form-control" id="assunto" placeholder="Assundo do e-mail">
-							</div>
-
-							<div class="form-group">
-								<label for="mensagem">Mensagem</label>
-								<textarea class="form-control" id="mensagem"></textarea>
-							</div>
-
-							<button type="submit" class="btn btn-primary btn-lg">Enviar Mensagem</button>
-						</form>
+			<div class="card-body font-weight-bold">
+				<form action="processa_envio.php" method="post">
+					<div class="form-group">
+						<label for="para">Para</label>
+						<input name="para" type="text" class="form-control" id="para" placeholder="joao@dominio.com.br">
 					</div>
-				</div>
-      		</div>
-      	</div>
+
+					<div class="form-group">
+						<label for="assunto">Assunto</label>
+						<input name="assunto" type="text" class="form-control" id="assunto" placeholder="Assundo do e-mail">
+					</div>
+
+					<div class="form-group">
+						<label for="mensagem">Mensagem</label>
+						<textarea name="mensagem" class="form-control" id="mensagem"></textarea>
+					</div>
+
+					<button type="submit" class="btn btn-primary btn-lg">Enviar Mensagem</button>
+				</form>
+			</div>
+		</div>
+    </div>
+</div>
 
 	</body>
 </html>
